35715d3a084d44121f4ae97deeecb4e56d4933ef
Xavier Georget <xageorget@gmail.com>
2023-06-16 13:08:35 +0200

    .env removed

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,8 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
   const headers = {
-    "X-RapidAPI-Key": "ed4ea1c573msh4e63bc7035b0883p1b220bjsn8a83c170a842",
+    "X-RapidAPI-Key": `${process.env.NEXT_PUBLIC_RAPID_API_Key}`,
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
@@ -29,4 +31,21 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL(`https://cdn.imagin.studio/getImage`);
+
+  const { make, year, model } = car;
+  url.searchParams.append(
+    "customer",
+    `${process.env.NEXT_PUBLIC_IMAGIN_STUDIO_KEY}`
+  );
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
 };
